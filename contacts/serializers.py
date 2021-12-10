0b0b6afe5ba7fcbd4683cfce4f364f22fdb5fdf7
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2021-12-10 18:48:37 +0200

    Fixed the contact_picture field name

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -6,4 +6,4 @@ class ContactSerializer(ModelSerializer):
     class Meta:
         model = Contact
         fields = ['country_code', 'first_name', 'last_name', 'phone_number',
-                  '[contact_picture', 'is_favorite']
+                  'contact_picture', 'is_favorite']
